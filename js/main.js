29600d5a572421242835b7d17d670bcbf7ece3dd
devBelenMG <moonbelen5@gmail.com>
2024-10-12 20:23:13 -0600

    se integro la carpeta del sistema con archivos

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function validar(){
+
+    var nombre = document.getElementById^['Nombre'];
+    var Ap = document.getElementById['Ap'];
+    var am = document.getElementById['Am'];
+    var email = document.getElementById['Email'];
+    var pass = document.getElementById['Pass'];
+    var tel = document.getElementById['Telefono'];
+    var grupo = document.getElementById['Grupo'];
+
+    if(nombre == null){
+        alert ('el campo nombre esta vacío');
+    }
+   /* else if(Ap == ' '){
+        alert ("el campo Apellido Paterno Esata Vacio");
+    }
+    else if(am == ' '){
+        alert("el campo Apellido Paterno Esata Vacio");
+    }
+    else if(email == ' '){
+        alert ("el campo Apellido Paterno Esata Vacio");
+    }
+    else if(pass ==' '){
+        alert ("el campo Apellido Paterno Esata Vacio");
+    }
+    else if(tel == ' '){
+        alert ("el campo Apellido Paterno Esata Vacio");
+    }
+    else if (grupo == " "){
+
+        alert ("el campo grupo");
+    }*/
+
+}
