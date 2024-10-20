4965e4e7ccc0fbe48fe19c603d3e76be2672247e
devBelenMG <moonbelen5@gmail.com>
2024-10-19 21:09:32 -0600

    se agrego nuevo contenido al archivo index

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>INICIO</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Inicio | Abilitytalk</title>
 </head>
 <body>
-    <h1>HOLA MUNDO</h1>
+<!-- inicio del contenedor de la pagina web-->
+    <div class="container d-flex justify-content-center aling-items-center">
+        <div class="col-md-10 col-lg-6 px-2 mt-3">
+            <h3 class="text-center mb-4 mt-3">inicio de sesion</h3>
+
+        </div>
+    </div>
 </body>
 </html>
