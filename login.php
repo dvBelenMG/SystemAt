4765e288d218cde50e8afde2b2d386bf11cc6cfc
devBelenMG <moonbelen5@gmail.com>
2024-11-17 17:48:09 -0600

    guardar cambios recientes

---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container mt-5 shadow">
+        <div class="row mt-2">
+        <div class="col-sm-10 col-md-5 col-lg-5 text-center">
+        <div class="text-center">
+             <img src="img/logAT.png" class="img-fluid mt-4" width = "200px" alt="...">
+            </div>
+        </div>
+
+        <div class="col-sm-10 col-md-5 col-lg-5 text-center">
+        <div class="col-sm-12 col-md-12 col-lg-12 justify-content-center text-center">
+            
+        <h2>Registro de usuario</h2>
+
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-12 justify-content-center    ">
+            
+            <input type="email" name="" id="" placeholder ="Email" class=" inp form-control mt-3">
+
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-12 text-center">
+            
+            <input type="email" name="" id="" placeholder ="Email" class=" inp form-control mt-2">
+
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-12 text-center">
+            
+        <input type="submit" value="registrar" name="registrar" class="boton form-control  mt-3 text-light">
+
+        </div>
+            
+            
+
+        </div>
+        
+
+        </div>
+    </div>
+</body>
+</html>
