3c59fd615a242905d11878ae2c20a7bcc614a018
devBelenMG <moonbelen5@gmail.com>
2024-11-18 19:09:07 -0600

    cambios en el dashboard

--- login.php
+++ login.php
@@ -8,18 +8,19 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="container mt-5 shadow">
+    <div class="container mt-5 justify-content-center align-items-center">
+        <div class=" cont2 col-sm-12 col-md-12 col-lg-12 justify-content-center align-items-center shadow mt-4">
         <div class="row mt-2">
         <div class="col-sm-10 col-md-5 col-lg-5 text-center">
         <div class="text-center">
-             <img src="img/logAT.png" class="img-fluid mt-4" width = "200px" alt="...">
+             <img src="img/logAT.png" class="img-fluid mt-5 mb-4" width = "200px" alt="...">
             </div>
         </div>
 
         <div class="col-sm-10 col-md-5 col-lg-5 text-center">
-        <div class="col-sm-12 col-md-12 col-lg-12 justify-content-center text-center">
+        <div class="col-sm-12 col-md-12 col-lg-12 justify-content-center text-center mb-2 mt-5">
             
-        <h2>Registro de usuario</h2>
+        <h2>Inicio de sesión</h2>
 
         </div>
         <div class="col-sm-12 col-md-12 col-lg-12 justify-content-center    ">
@@ -29,12 +30,18 @@
         </div>
         <div class="col-sm-12 col-md-12 col-lg-12 text-center">
             
-            <input type="email" name="" id="" placeholder ="Email" class=" inp form-control mt-2">
+            <input type="password" name="pass" id="Pass" placeholder ="contraseña" class=" inp form-control mt-2">
 
         </div>
         <div class="col-sm-12 col-md-12 col-lg-12 text-center">
             
         <input type="submit" value="registrar" name="registrar" class="boton form-control  mt-3 text-light">
+
+        </div>
+
+        <div class="col-sm-12 col-md-12 col-lg-12 text-center">
+            
+        <nav>no tienes una cuenta? <a href="">registrate</a></nav>
 
         </div>
             
@@ -43,6 +50,7 @@
         </div>
         
 
+        </div>
         </div>
     </div>
 </body>
